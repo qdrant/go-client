733f5fc277eaf51656f1d8cefaf6e5554a93137e
Robert-Stam <47088745+Robert-Stam@users.noreply.github.com>
2025-07-29 17:34:24 +0200

    Add support for connection pool (transparent for the user) (#81)
    * Add support for connection pool (transparent for the user)
    
    The default is don't use the pool, like now.
    
    * Make it uint, so it cannot be negative.
    
    * added comments
    
    * Default is 3

--- qdrant/client.go
+++ qdrant/client.go
@@ -1,76 +1,115 @@
 package qdrant
 
 import (
+	"fmt"
+	"sync/atomic"
+
 	"google.golang.org/grpc"
 )
 
-// High-level client for interacting with a Qdrant server.
+// Client is a high-level client for Qdrant.
+// It can manage a single connection or a pool of connections, chosen by setting
+// PoolSize in the Config.
 type Client struct {
-	grpcClient *GrpcClient
+	clients []*GrpcClient
+	next    uint32
 }
 
-// Instantiates a new client with the given configuration.
+// NewClient creates a new Qdrant client.
+// It checks Config.PoolSize to determine whether to create a single client
+// or a pool of clients. If PoolSize > 1, requests are distributed across
+// the connections in a round-robin fashion.
 func NewClient(config *Config) (*Client, error) {
-	grpcClient, err := NewGrpcClient(config)
-	if err != nil {
-		return nil, err
+	// Ensure config is not modified for the caller by cloning.
+	cfgCopy := *config
+	// Set default values
+	if cfgCopy.PoolSize == 0 {
+		cfgCopy.PoolSize = 3
 	}
-	return NewClientFromGrpc(grpcClient), nil
+	// Create the client, with an inner connection pool of go grpc clients
+	client := &Client{
+		clients: make([]*GrpcClient, 0, cfgCopy.PoolSize),
+	}
+	// Iterate over the pool size to create the individual client.
+	for i := range cfgCopy.PoolSize {
+		if i > 0 {
+			// In case of a pool, we only want to check compatibility once.
+			cfgCopy.SkipCompatibilityCheck = true
+		}
+		grpcClient, err := NewGrpcClient(&cfgCopy)
+		if err != nil {
+			// Close already opened clients before returning an error
+			client.Close()
+			return nil, fmt.Errorf("failed to create client %d in pool: %w", i, err)
+		}
+		client.clients = append(client.clients, grpcClient)
+	}
+	// Return the client
+	return client, nil
 }
 
 // Instantiates a new client with the default configuration.
 // Connects to localhost:6334 with TLS disabled.
 func DefaultClient() (*Client, error) {
-	grpcClient, err := NewDefaultGrpcClient()
-	if err != nil {
-		return nil, err
-	}
-	return NewClientFromGrpc(grpcClient), err
+	return NewClient(&Config{})
 }
 
-// Instantiates a new client from an existing gRPC client.
-func NewClientFromGrpc(grpcClient *GrpcClient) *Client {
-	return &Client{
-		grpcClient,
+// get returns the next GrpcClient from the pool in a round-robin fashion.
+func (c *Client) get() *GrpcClient {
+	if len(c.clients) == 1 {
+		return c.clients[0]
 	}
+	// Atomically increment and wrap around the counter
+	idx := atomic.AddUint32(&c.next, 1) - 1
+	return c.clients[idx%uint32(len(c.clients))]
 }
 
-// Get the underlying gRPC client.
+// Get the underlying gRPC client. In case of a pool, it returns one of the clients
+// in a round-robin fashion.
 func (c *Client) GetGrpcClient() *GrpcClient {
-	return c.grpcClient
+	return c.get()
 }
 
 // Get the low-level client for the collections gRPC service.
 // https://github.com/qdrant/qdrant/blob/master/lib/api/src/grpc/proto/collections_service.proto
 func (c *Client) GetCollectionsClient() CollectionsClient {
-	return c.GetGrpcClient().Collections()
+	return c.get().Collections()
 }
 
 // Get the low-level client for the points gRPC service.
 // https://github.com/qdrant/qdrant/blob/master/lib/api/src/grpc/proto/points_service.proto
 func (c *Client) GetPointsClient() PointsClient {
-	return c.GetGrpcClient().Points()
+	return c.get().Points()
 }
 
 // Get the low-level client for the snapshots gRPC service.
 // https://github.com/qdrant/qdrant/blob/master/lib/api/src/grpc/proto/snapshots_service.proto
 func (c *Client) GetSnapshotsClient() SnapshotsClient {
-	return c.GetGrpcClient().Snapshots()
+	return c.get().Snapshots()
 }
 
 // Get the low-level client for the Qdrant gRPC service.
 // https://github.com/qdrant/qdrant/blob/master/lib/api/src/grpc/proto/qdrant.proto
 func (c *Client) GetQdrantClient() QdrantClient {
-	return c.GetGrpcClient().Qdrant()
+	return c.get().Qdrant()
 }
 
-// Get the underlying *grpc.ClientConn.
+// GetConnection returns one of the underlying gRPC connections from the pool.
+// Useful for debugging or advanced use cases.
 func (c *Client) GetConnection() *grpc.ClientConn {
-	return c.GetGrpcClient().Conn()
+	return c.get().Conn()
 }
 
+// Close tears down all underlying connections.
 func (c *Client) Close() error {
-	return c.grpcClient.Close()
+	var lastErr error
+	for _, client := range c.clients {
+		if err := client.Close(); err != nil {
+			lastErr = err
+		}
+	}
+	c.clients = nil // Clear the slice
+	return lastErr
 }
 
 // Creates a pointer to a value of any type.
